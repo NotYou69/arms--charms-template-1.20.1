26aa641315a80fe3cfbb7ded7326eea61925791c
NotYou69 <jameswingrove@hotmail.co.uk>
2024-07-02 21:23:10 +0100

    importing

---
+++ src/main/java/net/james/armsandcharms/ArmsAndCharmsClient.java
@@ -0,0 +1,10 @@
+package net.james.armsandcharms;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArmsAndCharmsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/james/armsandcharms/ArmsAndCharmsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.james.armsandcharms;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ArmsAndCharmsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
